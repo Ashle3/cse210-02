33fead43572ce4e8bd55a241bac8679c90095efe
Sithlordbrock <brock.ford2002@gmail.com>
2022-10-06 21:56:23 -0600

    last touches

--- _main_.py
+++ _main_.py
@@ -6,7 +6,6 @@ def main():
     playersName = input("What is your name?: ")
     player1 = Player()
     player1.name = playersName
-<<<<<<< HEAD
     cardDeck = Deck()
     
     currCard = cardDeck.drawCard()
@@ -38,58 +37,10 @@ def main():
         keepPlaying = input("Would you like to keep playing? (type \"quit\" to quit): ")
         if keepPlaying == "quit":
             player1.is_playing = False
-=======
-    cardDeck = deck()
-    cardSelection = cardDeck.drawCard()
-
-    while player1.is_playing == True: 
-        nextCard = cardDeck.drawCard()
-        print(f"The current card is {cardSelection.number} of {cardSelection.suit}")
-
-        #validate that input
-        validated = False
-        while not validated:
-            question = input("Do you think the next one is Higher or Lower? (h/l): ")
-            if question == "h":
-                validated = True
-                result = cardSelection.cardCompareHigher(nextCard)
-            elif question == "l":
-                validated = True
-                result = cardSelection.cardCompareLower(nextCard)
-            else:
-                print("Invalid Input. Try again. ")
-        print(f"The next card drawn is {nextCard.number} of {nextCard.suit}. ")
-        player1.changePlayerScore(result)
-        player1.checkPlayerPoints()
-        
-        print(f"You currently have {player1.score_start} points. ")
-        validatedKeepPlaying = False
-        while not validatedKeepPlaying:
-            answer = input("Do you want to keep playing? y/n: ")
-            if answer == "n":
-                validatedKeepPlaying = True
-                player1.is_playing = False
-            elif answer == "y":
-                validatedKeepPlaying = True
-            else:
-                print("Invalid Answer")
-        cardSelection = nextCard
->>>>>>> main
 
     print("Thanks for playing the game!")
-<<<<<<< HEAD
     print(f"{player1.name}, you ended the game with {player1.score} points.")
-=======
-    print(f"You ended the game with {player1.score_start}")
->>>>>>> main
 
 
-<<<<<<< HEAD
-=======
-
-
-
-        
->>>>>>> main
 
 main()
